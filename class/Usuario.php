c71ff52eed72331cc1f071a666a13107bb384634
Jereelton de Oliveira Teixeira <jereelton@hotmail.com>
2019-10-30 23:13:36 -0300

    Novos metodos inseridos: UPDATE e DELETE

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,48 +1,169 @@
 <?php
 
-class Sql extends PDO {
+class Usuario {
 
-	private $conn;
+	private $idusuario;
+	private $deslogin;
+	private $dessenha;
+	private $dtcadastro;
 
-	public function __construct() {
-
-		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "production", "123mudar");
-
+	public function getIdUsuario() {
+		return $this->idusuario;
 	}
 
-	private function setParams($statment, $parameters = array()) {
+	public function setIdUsuario($value) {
+		$this->idusuario = $value;
+	}
 
-		foreach ($parameters as $key => $value) {
-			
-			$this->setParam($key, $value);
+	public function getDeslogin() {
+		return $this->deslogin;
+	}
+
+	public function setDeslogin($value) {
+		$this->deslogin = $value;
+	}
+
+	public function getDessenha() {
+		return $this->dessenha;
+	}
+
+	public function setDessenha($value) {
+		$this->dessenha = $value;
+	}
+
+	public function getDtCadastro() {
+		return $this->dtcadastro;
+	}
+
+	public function setDtCadastro($value) {
+		$this->dtcadastro = $value;
+	}
+
+	public function loadById($id) {
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+			":ID" => $id
+		));
+
+		if(count($results) > 0) {
+
+			$this->setData($results[0]);
 
 		}
 
 	}
 
-	private function setParam($statment, $key, $value) {
+	public static function getList() {
 
-		$statment->bindParam($key, $value);
+		$sql = new Sql();
 
-	}
-
-	public function query($rawQuery, $params = array()) {
-
-		$stmt = $this->conn->prepare($rawQuery);
-
-		$this->setParams($stmt, $params);
-
-		$stmt->execute();
-
-		return $stmt;
+		return $sql->select("SELECT * FROM tb_usuarios");
 
 	}
 
-	public function select($rawQuery, $params = array()):array {
+	public static function search($login) {
 
-		$stmt = $this->query($rawQuery, $params);
+		$sql = new Sql();
 
-		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+		));
+
+	}
+
+	public function login($login, $password) {
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN" => $login,
+			":PASSWORD"=> $password
+		));
+
+		if(count($results) > 0) {
+
+			$this->setData( $results[0]);
+
+		} else {
+
+			throw new Exception("Login ou Senha invalido");
+			
+		}
+
+	}
+
+	public function setData($data) {
+
+		$this->setIdUsuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtCadastro(new DateTime($data['dtcadastro']));
+
+	}
+
+	public function insert() {
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+
+		if(count($results) > 0) {
+
+			$this->setData($results[0]);
+
+		}
+
+	}
+
+	public function update($login, $password) {
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha(),
+			':ID'=>$this->getIdUsuario()
+		));
+	}
+
+	public function delete() {
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+			':ID'=>$this->getIdUsuario()
+		));
+
+		$this->setIdUsuario(0);
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtCadastro(new DateTime());
+
+	}
+
+	public function __construct($login = "", $password = "") {
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+
+	}
+
+	public function __toString() {
+
+		return json_encode(array(
+			"idusuario"=>$this->getIdUsuario(),
+			"deslogin"=>$this->getDeslogin(),
+			"dessenha"=>$this->getDessenha(),
+			"dtcadastro"=>$this->getDtCadastro()->format("d-m-Y H:i:s")
+		));
 
 	}
 
--- index.php
+++ index.php
@@ -2,10 +2,54 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+/*$sql = new Sql();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+$root = new Usuario();
+$root->loadById(12);
 
-echo json_encode($usuarios);
+echo $root;*/
+
+/*$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+echo json_encode($usuarios);*/
+
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+/*$search = Usuario::search('ad');
+
+echo json_encode($search);*/
+
+/*$usuario = new Usuario();
+$usuario->login("root", "root");
+
+echo $usuario;*/
+
+/*$aluno = new Usuario("aluno_".rand(), md5(rand()));
+
+$aluno->insert();
+
+echo $aluno;*/
+
+/*$usuario = new Usuario();
+$usuario->loadById(12);
+$usuario->update("professor", md5(rand()));
+
+echo $usuario;*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(21);
+$usuario->delete();
+
+echo $usuario;
 
 ?>
+
+<style type="text/css">
+	body {
+		background-color: #000000;
+		color: #FFFFFF;
+	}
+</style>
